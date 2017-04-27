86ef32db6324d5a78da56c7460ad2a67152590a8
Pretcorp2 <pretcorp2@gmail.com>
2017-04-27 09:32:53 -0600

    Delete codigo1.py

--- codigo1.py
+++
@@ -1,4 +0,0 @@
-print("Bievenidos a la calculadora de 8 numeros, ingrese a continuacion 'sumvalores8' y entre parentesis las cantidades que desea sumar separados por una coma *El Limite son 8 cantidades")
-def sumvalores8(n1,n2,n3,n4,n5,n6,n7,n8):
-        print("La Suma de los valores es: ", n1+n2+n3+n4+n5+n6+n7+n8)
-        print("Los numeros eran: ", n1,n2,n3,n4,n5,n6,n7,n8)

5b88f1e06185df0cf2c84c70b59d8329c1e8eadd
Pretcorp2 <pretcorp2@gmail.com>
2017-04-27 06:53:57 -0600

    Rename codigo1.py to SCejer1.py

---
+++ SCejer1.py
@@ -0,0 +1,10 @@
+print("Vamos a evaluar un numero para ver si es par o impar")
+x=int(input("Ingrese el numero que desee evaluar"));
+
+if ((x%2)==0):
+  print("El Numero que ingresaste en par")
+elif ((x%3)==0):
+  print("El Numero que ingresaste es impar")
+else:
+  print("Ingresaste un numero primo")
+  
